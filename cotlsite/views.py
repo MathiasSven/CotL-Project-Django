37dce1d87cf9e179b1baef79522859b73567af49
Mathias Sven <mathiassven2@hotmail.com>
2020-11-03 09:59:59 -0300

    Possible Style Changes

--- cotlsite/views.py
+++ cotlsite/views.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from discordlogin.models import User
 
 
-def index(request):
-    return render(request, "cotlsite/index.html")
+def home(request):
+    return render(request, "cotlsite/home.html")
+
+
+def events_views(request):
+    pass
+
+
+def members_view(request):
+    pass
+
+
+@login_required
+def tools_view(request):
+    pass
