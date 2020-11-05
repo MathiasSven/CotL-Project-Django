bb2a3b8a12ac360a163c37694fec6b58921f1823
Mathias Sven <mathiassven2@hotmail.com>
2020-11-05 07:26:25 -0300

    Removed SECRET

--- cotlproject/settings.py
+++ cotlproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6+ie%4g^=78kts$4(v@8v$96syc!gl5x&xzi#t%(6s2ovfb4q$'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
