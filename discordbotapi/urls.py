1e68247a3497d12576b82a7d7a14dd2d64192647
Mathias Sven <mathiassven2@hotmail.com>
2020-11-08 10:52:14 -0300

    Ok NOW API is Fully working

--- discordbotapi/urls.py
+++ discordbotapi/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("member-remove", views.member_remove, name="member_remove"),
     path("member-update", views.member_update, name="member_update"),
     path("user-update", views.user_update, name="user_update"),
+    path("role-create", views.role_create, name="role_create"),
+    path("role-remove", views.role_remove, name="role_remove"),
+    path("role-update", views.role_update, name="role_update"),
     path("members-bulk", views.members_bulk, name="members_bulk"),
 ]
--- discordbotapi/views.py
+++ discordbotapi/views.py
@@ -7,15 +7,6 @@ from .models import APIKey
 from cotlsite.models import Member, Role
 
 
-# class ApiIO(object):
-#     def __init__(self, _id, name=None, discriminator=None, avatar=None, nick=None):
-#         self.id = _id
-#         self.name = name
-#         self.discriminator = discriminator
-#         self.avatar = avatar
-#         self.nick = nick
-
-
 @csrf_exempt
 def member_join(request):
     if request.method == 'POST':
@@ -32,7 +23,7 @@ def member_join(request):
                 tmp_role = Role.objects.get(id=role['id'])
                 tmp_member.roles.add(tmp_role)
             return JsonResponse({
-                "POST": "Successful"
+                "POST": "Member creation successful"
             }, status=201)
         else:
             return JsonResponse({
@@ -46,12 +37,12 @@ def member_join(request):
 
 @csrf_exempt
 def member_remove(request):
-    if request.method == 'DELETE':
+    if request.method == 'PUT':
         if APIKey.check_key(request.headers['X-Api-Key']):
             member = json.loads(request.body)
             Member.objects.get(id=member['id']).delete()
             return JsonResponse({
-                "DELETE": "Successful"
+                "PUT": "Member deletion successful"
             }, status=200)
         else:
             return JsonResponse({
@@ -59,7 +50,7 @@ def member_remove(request):
             }, status=403)
     else:
         return JsonResponse({
-            "error": "DELETE request required."
+            "error": "PUT request required."
         }, status=405)
 
 
@@ -68,24 +59,24 @@ def member_update(request):
     if request.method == 'PUT':
         if APIKey.check_key(request.headers['X-Api-Key']):
             member = json.loads(request.body)
-            tmp_member = Member.objects.get(member['id'])
-            tmp_member.roles.clear()
-
-            tmp_member.name = member['name']
-            tmp_member.discriminator = member['discriminator']
-            tmp_member.avatar = member['avatar']
+            tmp_member = Member.objects.get(id=member['id'])
             tmp_member.nick = member['nick']
             tmp_member.save()
-            for role in member['roles']:
-                tmp_role, _ = Role.objects.get_or_create(
-                    id=role['id'],
-                    name=role['name'],
-                    position=role['position'],
-                    colour=f"#{hex(role['colour']).lstrip('0x')}"
-                )
-                tmp_member.roles.add(tmp_role)
+            try:
+                roles = member['roles']
+                tmp_member.roles.clear()
+                for role in roles:
+                    tmp_role, _ = Role.objects.get_or_create(id=role['id'])
+                    tmp_role.name = role['name']
+                    tmp_role.position = role['position']
+                    tmp_role.colour = f"#{hex(role['colour']).lstrip('0x')}"
+                    tmp_role.save()
+                    tmp_member.roles.add(tmp_role)
+            except KeyError:
+                pass
+
             return JsonResponse({
-                "POST": "Successful"
+                "PUT": "Member update successful"
             }, status=200)
         else:
             return JsonResponse({
@@ -93,7 +84,7 @@ def member_update(request):
             }, status=403)
     else:
         return JsonResponse({
-            "error": "PATCH request required."
+            "error": "PUT request required."
         }, status=405)
 
 
@@ -102,13 +93,13 @@ def user_update(request):
     if request.method == 'PUT':
         if APIKey.check_key(request.headers['X-Api-Key']):
             member = json.loads(request.body)
-            tmp_member = Member.objects.get(member['id'])
+            tmp_member = Member.objects.get(id=member['id'])
             tmp_member.name = member['name']
             tmp_member.discriminator = member['discriminator']
             tmp_member.avatar = member['avatar']
             tmp_member.save()
             return JsonResponse({
-                "POST": "Successful"
+                "PUT": "User update successful"
             }, status=200)
         else:
             return JsonResponse({
@@ -116,7 +107,73 @@ def user_update(request):
             }, status=403)
     else:
         return JsonResponse({
-            "error": "PATCH request required."
+            "error": "PUT request required."
+        }, status=405)
+
+
+@csrf_exempt
+def role_create(request):
+    if request.method == 'POST':
+        if APIKey.check_key(request.headers['X-Api-Key']):
+            role = json.loads(request.body)
+            Role.objects.create(
+                id=role['id'],
+                name=role['name'],
+                position=role['position'],
+                colour=f"#{hex(role['colour']).lstrip('0x')}"
+            )
+            return JsonResponse({
+                "POST": "Member creation successful"
+            }, status=201)
+        else:
+            return JsonResponse({
+                "error": "Wrong or no API Key provided"
+            }, status=403)
+    else:
+        return JsonResponse({
+            "error": "POST request required."
+        }, status=405)
+
+
+@csrf_exempt
+def role_remove(request):
+    if request.method == 'PUT':
+        if APIKey.check_key(request.headers['X-Api-Key']):
+            role = json.loads(request.body)
+            Role.objects.get(id=role['id']).delete()
+            return JsonResponse({
+                "PUT": "Role removal successful"
+            }, status=200)
+        else:
+            return JsonResponse({
+                "error": "Wrong or no API Key provided"
+            }, status=403)
+    else:
+        return JsonResponse({
+            "error": "PUT request required."
+        }, status=405)
+
+
+@csrf_exempt
+def role_update(request):
+    if request.method == 'PUT':
+        if APIKey.check_key(request.headers['X-Api-Key']):
+            role = json.loads(request.body)
+            tmp_role = Role.objects.get(id=role['id'])
+            tmp_role.name = role['name']
+            tmp_role.position = role['position']
+            tmp_role.colour = f"#{hex(role['colour']).lstrip('0x')}"
+            tmp_role.save()
+            return JsonResponse({
+                "PUT": "Successful"
+            }, status=200)
+        else:
+            return JsonResponse({
+                "error": "Wrong or no API Key provided"
+            }, status=403)
+    else:
+        return JsonResponse({
+            "error": "PUT request required."
         }, status=405)
 
 
