e65131151cd7a412f81bb14557b929ab2fc6c07c
Mathias Sven <mathiassven2@hotmail.com>
2020-11-21 23:48:13 -0300

    Changed PnWData verbose name

--- cotlsite/admin.py
+++ cotlsite/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Member, Role
+from .models import Member, Role, PnWData
 
 admin.site.register(Member)
 admin.site.register(Role)
+admin.site.register(PnWData)
