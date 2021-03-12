e220bf6b82d6d3eb40bfe232164c2c9fe870dc06
Mathias Sven <mathiassven2@hotmail.com>
2021-03-12 01:53:31 -0300

    Hotfix

--- pnwdata/apps.py
+++ pnwdata/apps.py
@@ -9,7 +9,7 @@ class PnwdataConfig(AppConfig):
     def ready(self):
         super(PnwdataConfig, self).ready()
 
-        if not ('migrate' in sys.argv):
+        if not ('migrate' in sys.argv or 'makemigrations' in sys.argv):
             from .models import Market, AllianceConfig
             for resource in Market.RESOURCE_TYPE:
                 if not Market.objects.filter(pk=resource[0]).exists():
