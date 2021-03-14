0b52ffd9154cc6443219797289d931f33407d35d
Mathias Sven <mathiassven2@hotmail.com>
2021-03-14 02:46:45 -0300

    Step in mobile ready

--- cotlsite/static/cotlsite/js/home.js
+++ cotlsite/static/cotlsite/js/home.js
@@ -1,4 +1,7 @@
 const audio = document.getElementById('anthem');
+const hamburger = document.querySelector(".hamburger");
+let nav = document.querySelector(".nav");
+let blur_overlay = document.querySelector(".blur-overlay");
 
 function isPlaying(playerId) {
     let player = document.getElementById(playerId);
@@ -28,4 +31,19 @@ document.getElementById("play-pause-button").addEventListener("click", function 
     document.getElementById("pause-button").style.display = "flex"
   }
 
+});
+
+const toggle_menu_opened = () => {
+    hamburger.classList.toggle("open");
+    nav.classList.toggle("open");
+    blur_overlay.classList.toggle("open");
+}
+
+hamburger.addEventListener("click", (e) => {
+    toggle_menu_opened()
+
+    e.stopPropagation();
+    if (hamburger.classList.contains('open')) {
+        document.addEventListener('click', offClick);
+    }
 });
